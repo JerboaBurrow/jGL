48c60ddb5cb876a22e6bf9ec9add24e2e59ee867
Jerboa-app <dev@jerboa.app>
2023-10-30 08:46:24 +0000

    set opengl version compat with glsl 330

--- include/jGL/Display/display.h
+++ include/jGL/Display/display.h
@@ -54,7 +54,7 @@ namespace jGL
                     // required for MacOS
                     //   https://www.glfw.org/faq.html#41__how_do_i_create_an_opengl_30_context
                     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-                    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+                    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
                     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
                     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
                     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
