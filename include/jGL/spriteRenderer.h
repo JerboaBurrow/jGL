b7f09b1fbac5a2046daafdef319ac4025d3958e2
Jerboa-app <dev@jerboa.app>
2024-07-31 10:16:35 +0100

    Documents SpriteRenderer

--- include/jGL/spriteRenderer.h
+++ include/jGL/spriteRenderer.h
@@ -1,5 +1,5 @@
-#ifndef SPRITERENDERER
-#define SPRITERENDERER
+#ifndef SPRITERENDERER_H
+#define SPRITERENDERER_H
 
 #include <jGL/sprite.h>
 #include <jGL/shader.h>
@@ -12,36 +12,82 @@
 
 namespace jGL
 {
+    /** 
+     * @brief User name for a Sprite.
+     * @typedef SpriteId 
+     * */
     typedef std::string SpriteId;
 
-    /*
-    
-        Looks after sprites, and groups for batch rendering
-
-            If two sprites have the same texture (inc. atlas) they will be stored
-                together for batched rendering
-    
-    */
+    /**
+     * @brief Renders sprites in batches, with optional render priority.
+     * 
+     * @remark Currently there are 4 texture slots loaded at once.
+     * @remark Keeping to 4 textures is most efficient (1 draw call), atlas textures are useful for this.
+     * @remark RenderPriority of Sprites may lead to inefficient batching across textures. 
+     * Try to keep similar RenderPriority within the same texture/ group of 4 textures.
+     */
     class SpriteRenderer
     {
 
     public:
+
+        /**
+         * @brief Largest number of concurrent textures bound for one batch.
+         * 
+         */
+        static const uint8_t MAX_BATCH_BOUND_TEXTURES = MAX_SPRITE_BATCH_BOUND_TEXTURES;
     
+        /**
+         * @brief Construct a new SpriteRenderer.
+         * 
+         * @param sizeHint Reserve some memory for this many Sprites.
+         */
         SpriteRenderer(size_t sizeHint = 8)
         {
             sprites.reserve(sizeHint); 
-            textures.reserve(MAX_TEXTURE_SLOTS);
+            textures.reserve(MAX_BATCH_BOUND_TEXTURES);
         }
 
         Sprite & getSprite(SpriteId id);
 
         const Transform & getTransform(SpriteId id) { return getSprite(id).transform; }
-        const TextureOffset getTextureOffset(SpriteId id) { return getSprite(id).getTextureOffset(); }
 
+        /**
+         * @brief Get a Sprites TextureOffset
+         * @remark In pixel units, see TextureOffset::getTextureOffset()
+         * 
+         * @param id 
+         * @param normalised Return in normalised units.
+         * @return const TextureOffset 
+         */
+        const TextureOffset getTextureOffset(SpriteId id, bool normalised = false) { return getSprite(id).getTextureOffset(normalised); }
+
+        /**
+         * @brief Draw with overriding render priority and shader.
+         * 
+         * @param shader An OpenGL Shader to draw all the Sprites with.
+         * @param ids Render priorities for the Sprites.
+         */
         virtual void draw(std::shared_ptr<Shader> shader, std::multimap<RenderPriority, SpriteId> ids) = 0;
+
+        /**
+         * @brief Draw with overriding render priority.
+         * 
+         * @param ids Render priorities for the Sprites.
+         */
         virtual void draw(std::multimap<RenderPriority, SpriteId> ids) = 0;
         
+        /**
+         * @brief Draw with overriding shader.
+         * 
+         * @param shader An OpenGL Shader to draw all the Sprites with.
+         */
         virtual void draw(std::shared_ptr<Shader> shader) { draw(shader, ids); }
+
+        /**
+         * @brief Draw with default shader and priority.
+         * 
+         */
         virtual void draw() { draw(ids); }
 
         virtual void add(Sprite s, SpriteId id, RenderPriority priority = 0);
@@ -86,7 +132,6 @@ namespace jGL
 
     protected:
 
-        static const uint8_t MAX_TEXTURE_SLOTS = 4;
         std::vector<std::shared_ptr<Texture>> textures;
 
         std::unordered_map<SpriteId, Sprite> sprites;
@@ -99,4 +144,4 @@ namespace jGL
     };
 }
 
-#endif /* SPRITERENDERER */
+#endif /* SPRITERENDERER_H */
