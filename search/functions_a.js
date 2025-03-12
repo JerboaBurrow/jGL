f90989449ec60077ed6a9089d1190d167b9b4011
Jerboa-app <84378622+Jerboa-app@users.noreply.github.com>
2025-03-12 08:43:09 +0000

    Deploying to docs from @ JerboaBurrow/jGL@c5f0d0f92ce7566226f9cba06daf085c8e24e1d7 🚀

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['keycallback_0',['keyCallback',['../classjGL_1_1DesktopDisplay.html#a4bc7d1a99b485616a2d06dc8e81e348e',1,'jGL::DesktopDisplay']]],
-  ['keyhasevent_1',['keyHasEvent',['../classjGL_1_1DesktopDisplay.html#a8a21b31c37eb966efaee3c70141b1d28',1,'jGL::DesktopDisplay']]]
+  ['keyhasanyevents_1',['keyHasAnyEvents',['../classjGL_1_1DesktopDisplay.html#a8914bc661f72a6c7e273eb355d12ad19',1,'jGL::DesktopDisplay']]],
+  ['keyhasevent_2',['keyHasEvent',['../classjGL_1_1DesktopDisplay.html#a8a21b31c37eb966efaee3c70141b1d28',1,'jGL::DesktopDisplay']]]
 ];
