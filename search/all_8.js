52912e5c473406745cbc7561b213adad348db503
Jerboa-app <84378622+Jerboa-app@users.noreply.github.com>
2025-10-07 17:20:53 +0000

    Deploying to docs from @ JerboaBurrow/jGL@16c3e96ac1f164421b5d63b9f99fbdef9a56cabb 🚀

--- search/all_8.js
+++ search/all_8.js
@@ -10,11 +10,12 @@ var searchData=
   ['in_7',['in',['../structjGL_1_1BoundingBox.html#a77c3d52ccc8110698386a2fa81c352b2',1,'jGL::BoundingBox']]],
   ['incrementzoom_8',['incrementZoom',['../classjGL_1_1OrthoCam.html#a64ad682457ef072d32901af12da29052',1,'jGL::OrthoCam']]],
   ['info_9',['info',['../structPriorityStore_1_1Info.html#a2aaece9b354a53c3599e2a54dfe66aed',1,'PriorityStore::Info::Info()'],['../classjLog_1_1INFO.html',1,'jLog::INFO'],['../structPriorityStore_1_1Info.html#ac3bf793ee00bc8d57d6141f96012e988',1,'PriorityStore::Info::Info()'],['../structPriorityStore_1_1Info.html',1,'PriorityStore&lt; T &gt;::Info']]],
-  ['inputrate_10',['inputRate',['../classjGL_1_1Vulkan_1_1VertexBufferObject.html#ad962e16f24de6e6bc2bafa114dab3de8',1,'jGL::Vulkan::VertexBufferObject']]],
-  ['isclosing_11',['isClosing',['../classjGL_1_1jGLInstance.html#ab0768f0028143737c46a57494419911d',1,'jGL::jGLInstance']]],
-  ['iscompiled_12',['isCompiled',['../structjGL_1_1GL_1_1glShader.html#a0d646f9fea2f789cd6449eb2a7a0d56f',1,'jGL::GL::glShader']]],
-  ['iscomplete_13',['isComplete',['../structjGL_1_1Vulkan_1_1QueueFamilyIndices.html#afa72d952fafcbd2b9a412945eb0c7524',1,'jGL::Vulkan::QueueFamilyIndices']]],
-  ['ismsaa_14',['isMSAA',['../classjGL_1_1GL_1_1glDrawFramebuffer.html#a727991a7acf3f627b967ca7d6bbf7fb1',1,'jGL::GL::glDrawFramebuffer']]],
-  ['isopen_15',['isOpen',['../classjGL_1_1DesktopDisplay.html#a54de0468bf84d7ef59cb7b566ae43692',1,'jGL::DesktopDisplay']]],
-  ['isprogram_16',['isProgram',['../structjGL_1_1GL_1_1glShader.html#af20163bddf77f653d8023d7b15a3c48f',1,'jGL::GL::glShader']]]
+  ['initimgui_10',['initImgui',['../classjGL_1_1DesktopDisplay.html#af3f4bd2131cc88ab1f7189399dcf6133',1,'jGL::DesktopDisplay']]],
+  ['inputrate_11',['inputRate',['../classjGL_1_1Vulkan_1_1VertexBufferObject.html#ad962e16f24de6e6bc2bafa114dab3de8',1,'jGL::Vulkan::VertexBufferObject']]],
+  ['isclosing_12',['isClosing',['../classjGL_1_1jGLInstance.html#ab0768f0028143737c46a57494419911d',1,'jGL::jGLInstance']]],
+  ['iscompiled_13',['isCompiled',['../structjGL_1_1GL_1_1glShader.html#a0d646f9fea2f789cd6449eb2a7a0d56f',1,'jGL::GL::glShader']]],
+  ['iscomplete_14',['isComplete',['../structjGL_1_1Vulkan_1_1QueueFamilyIndices.html#afa72d952fafcbd2b9a412945eb0c7524',1,'jGL::Vulkan::QueueFamilyIndices']]],
+  ['ismsaa_15',['isMSAA',['../classjGL_1_1GL_1_1glDrawFramebuffer.html#a727991a7acf3f627b967ca7d6bbf7fb1',1,'jGL::GL::glDrawFramebuffer']]],
+  ['isopen_16',['isOpen',['../classjGL_1_1DesktopDisplay.html#a54de0468bf84d7ef59cb7b566ae43692',1,'jGL::DesktopDisplay']]],
+  ['isprogram_17',['isProgram',['../structjGL_1_1GL_1_1glShader.html#af20163bddf77f653d8023d7b15a3c48f',1,'jGL::GL::glShader']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -4,10 +4,11 @@ var searchData=
   ['in_1',['in',['../structjGL_1_1BoundingBox.html#a77c3d52ccc8110698386a2fa81c352b2',1,'jGL::BoundingBox']]],
   ['incrementzoom_2',['incrementZoom',['../classjGL_1_1OrthoCam.html#a64ad682457ef072d32901af12da29052',1,'jGL::OrthoCam']]],
   ['info_3',['info',['../structPriorityStore_1_1Info.html#ac3bf793ee00bc8d57d6141f96012e988',1,'PriorityStore::Info::Info(ElementId i, Priority p)'],['../structPriorityStore_1_1Info.html#a2aaece9b354a53c3599e2a54dfe66aed',1,'PriorityStore::Info::Info()']]],
-  ['isclosing_4',['isClosing',['../classjGL_1_1jGLInstance.html#ab0768f0028143737c46a57494419911d',1,'jGL::jGLInstance']]],
-  ['iscompiled_5',['isCompiled',['../structjGL_1_1GL_1_1glShader.html#a0d646f9fea2f789cd6449eb2a7a0d56f',1,'jGL::GL::glShader']]],
-  ['iscomplete_6',['isComplete',['../structjGL_1_1Vulkan_1_1QueueFamilyIndices.html#afa72d952fafcbd2b9a412945eb0c7524',1,'jGL::Vulkan::QueueFamilyIndices']]],
-  ['ismsaa_7',['isMSAA',['../classjGL_1_1GL_1_1glDrawFramebuffer.html#a727991a7acf3f627b967ca7d6bbf7fb1',1,'jGL::GL::glDrawFramebuffer']]],
-  ['isopen_8',['isOpen',['../classjGL_1_1DesktopDisplay.html#a54de0468bf84d7ef59cb7b566ae43692',1,'jGL::DesktopDisplay']]],
-  ['isprogram_9',['isProgram',['../structjGL_1_1GL_1_1glShader.html#af20163bddf77f653d8023d7b15a3c48f',1,'jGL::GL::glShader']]]
+  ['initimgui_4',['initImgui',['../classjGL_1_1DesktopDisplay.html#af3f4bd2131cc88ab1f7189399dcf6133',1,'jGL::DesktopDisplay']]],
+  ['isclosing_5',['isClosing',['../classjGL_1_1jGLInstance.html#ab0768f0028143737c46a57494419911d',1,'jGL::jGLInstance']]],
+  ['iscompiled_6',['isCompiled',['../structjGL_1_1GL_1_1glShader.html#a0d646f9fea2f789cd6449eb2a7a0d56f',1,'jGL::GL::glShader']]],
+  ['iscomplete_7',['isComplete',['../structjGL_1_1Vulkan_1_1QueueFamilyIndices.html#afa72d952fafcbd2b9a412945eb0c7524',1,'jGL::Vulkan::QueueFamilyIndices']]],
+  ['ismsaa_8',['isMSAA',['../classjGL_1_1GL_1_1glDrawFramebuffer.html#a727991a7acf3f627b967ca7d6bbf7fb1',1,'jGL::GL::glDrawFramebuffer']]],
+  ['isopen_9',['isOpen',['../classjGL_1_1DesktopDisplay.html#a54de0468bf84d7ef59cb7b566ae43692',1,'jGL::DesktopDisplay']]],
+  ['isprogram_10',['isProgram',['../structjGL_1_1GL_1_1glShader.html#af20163bddf77f653d8023d7b15a3c48f',1,'jGL::GL::glShader']]]
 ];
