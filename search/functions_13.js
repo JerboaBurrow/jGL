39f23f51a4aa6a04935008ff3cb7b80cc9c81369
Jerboa-app <84378622+Jerboa-app@users.noreply.github.com>
2025-10-09 18:58:36 +0000

    Deploying to docs from @ JerboaBurrow/jGL@0bb89413c0056160e1dcf11dc0cdfa2223847933 🚀

--- search/functions_13.js
+++ search/functions_13.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['text_0',['text',['../classjGL_1_1GL_1_1OpenGLInstance.html#acfdd55071cdc43efcb523c252651d01d',1,'jGL::GL::OpenGLInstance::text()'],['../classjGL_1_1Vulkan_1_1VulkanInstance.html#a054f00934f72e14a26f7d80771bb400a',1,'jGL::Vulkan::VulkanInstance::text()'],['../classjGL_1_1jGLInstance.html#a06719831130e8a45e53b2ddbffb35467',1,'jGL::jGLInstance::text()']]],
-  ['textrenderer_1',['textrenderer',['../classjGL_1_1GL_1_1TextRenderer.html#a39a507b1c3d118612bff79a616624d88',1,'jGL::GL::TextRenderer::TextRenderer()'],['../classjGL_1_1Vulkan_1_1TextRenderer.html#abca7ebea949d6b55509223f8842faefd',1,'jGL::Vulkan::TextRenderer::TextRenderer()']]],
+  ['textrenderer_1',['textrenderer',['../classjGL_1_1GL_1_1TextRenderer.html#a68ec406a9436ac5ab1345e45b3fbda05',1,'jGL::GL::TextRenderer::TextRenderer()'],['../classjGL_1_1Vulkan_1_1TextRenderer.html#abca7ebea949d6b55509223f8842faefd',1,'jGL::Vulkan::TextRenderer::TextRenderer()']]],
   ['texture_2',['Texture',['../classjGL_1_1Texture.html#a5e4216bd4fba77a592de73fad488fcbc',1,'jGL::Texture']]],
   ['texturedparticle_3',['texturedparticle',['../structjGL_1_1TexturedParticle.html#afdd277c80105aca28e224d079e0e4399',1,'jGL::TexturedParticle::TexturedParticle()=default'],['../structjGL_1_1TexturedParticle.html#af5465be113f963890f5d3186c02ef70f',1,'jGL::TexturedParticle::TexturedParticle(glm::vec4 p, glm::vec4 c, glm::vec4 t)']]],
   ['textureregion_4',['textureregion',['../structjGL_1_1TextureRegion.html#a32cbe1f23b65920e94ceeb186b90b2c2',1,'jGL::TextureRegion::TextureRegion(uint16_t x, uint16_t y, uint16_t w, uint16_t h)'],['../structjGL_1_1TextureRegion.html#af27bbe2bd3c1afd0901e42868d51ce98',1,'jGL::TextureRegion::TextureRegion()']]],
